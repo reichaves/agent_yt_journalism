b3833670f480055253e0a39d80445c9af984cf56
Reinaldo Chaves <reichaves@gmail.com>
2025-03-21 20:17:04 -0300

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,14 @@
+# Este arquivo está vazio intencionalmente para tornar o diretório 'tools' um pacote Python
+# Isso permite que os módulos neste diretório sejam importados em outros lugares do código
 
+from tools.youtube_transcriber import YouTubeTranscriberTool
+from tools.web_search import WebSearchTool
+from tools.rag_query import RAGQueryTool
+from tools.journalistic_highlight import JournalisticHighlightTool
+
+__all__ = [
+    'YouTubeTranscriberTool',
+    'WebSearchTool',
+    'RAGQueryTool',
+    'JournalisticHighlightTool'
+]
